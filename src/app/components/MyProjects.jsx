a55edf6fdba1c1cd3518fa0f969ca79b5d512089
Doston <dastkraximoff@gmail.com>
2024-08-04 21:33:39 +0500

    filter added

--- src/app/components/MyProjects.jsx
+++ src/app/components/MyProjects.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { act, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
 import { motion } from "framer-motion";
@@ -42,22 +44,46 @@ const MyProjectsList = [
 ];
 
 export default function MyProjects() {
+  const [activeType, setActiveType] = useState("all");
+
+  const filteredProjects = MyProjectsList.filter((project) => {
+    if (activeType === "all") {
+      return true;
+    }
+    return project.type === activeType;
+  });
+
   return (
     <main id="projects" className="w-full min-h-screen">
       <h1 className="text-center font-bold text-4xl mb-5">My Projects</h1>
       <div className="flex gap-3 justify-center mt-[80px] mb-2">
-        <button className="text-white border-slate-600 hover:border-white rounded-full border-2 px-6 py-3 text-xl cursor-pointer">
+        <button
+          className={`text-white border-slate-600 hover:border-white rounded-full border-2 px-6 py-3 text-xl cursor-pointer ${
+            activeType === "all" ? "" : ""
+          }`}
+          onClick={() => setActiveType("all")}
+        >
           All
         </button>
-        <button className="text-white border-slate-600 hover:border-white rounded-full border-2 px-6 py-3 text-xl cursor-pointer">
+        <button
+          className={`text-white border-slate-600 hover:border-white rounded-full border-2 px-6 py-3 text-xl cursor-pointer ${
+            activeType === "all" ? "" : ""
+          }`}
+          onClick={() => setActiveType("web")}
+        >
           Web
         </button>
-        <button className="text-white border-slate-600 hover:border-white rounded-full border-2 px-6 py-3 text-xl cursor-pointer">
+        <button
+          className={`text-white border-slate-600 hover:border-white rounded-full border-2 px-6 py-3 text-xl cursor-pointer ${
+            activeType === "all" ? "" : ""
+          }`}
+          onClick={() => setActiveType("mobile")}
+        >
           Mobile
         </button>
       </div>
       <section className="flex flex-col sm:grid sm:grid-cols-3 space-y-5 items-center pb-5">
-        {MyProjectsList.map((project) => (
+        {filteredProjects.map((project) => (
           <motion.div
             initial={{ opacity: 0, scale: 0.5 }}
             animate={{ opacity: 1, scale: 1 }}
